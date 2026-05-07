edb8a0707ce1dce22e21a6f23582cb30058158ae
insaseemab <insaseemab212@gmail.com>
2026-05-08 00:00:54 +0500

    fix

--- lib/screens/register_screen/register.dart
+++ lib/screens/register_screen/register.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:get_storage/get_storage.dart'; // ← add this import
 import 'package:insaafconnect/core/services/auth_services.dart';
 import 'package:insaafconnect/routes/app_routes.dart';
 
